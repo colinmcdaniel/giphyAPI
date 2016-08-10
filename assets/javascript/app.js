ccbe8c4cee0c0207693a7241da31722a63c879e8
Colin McDaniel <colinmcdaniel@MacBook-Pro.local>
2016-08-09 18:00:57 -0700

    Secure URL

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,7 +26,7 @@ function renderButtons(){
 // When click topic button...
 $(document).on("click","button", function(){
 	var topic = $(this).data("name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 	$.ajax({
 		url: queryURL,
